3507eea46ed7966206da7de347c2b1a94cdb4365
RizwanDev-crypto <rizwanhayyat.dev@gmail.com>
2026-01-17 19:34:10 +0500

    Fix: Remove node_modules from repo

--- src/app/customer/login/page.js
+++
@@ -1,7 +0,0 @@
-// src/app/customer/login/page.js
-'use client';
-import AgentsLoginPage from "@/app/agents/login/page";
-
-export default function CustomerLoginPage() {
-  return <AgentsLoginPage />;
-}
